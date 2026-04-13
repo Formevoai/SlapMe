303f45e4d4c247f5ee11c488c33d2d0da6b87c0a
Emin Sahan <eminsahan@MacBook-Pro-von-Emin.fritz.box>
2026-04-14 01:31:42 +0200

    feat: rename prank to share.formevo.app/gift/

--- SlapMe/Features/Prank/PrankShareSheet.swift
+++ SlapMe/Features/Prank/PrankShareSheet.swift
@@ -124,12 +124,12 @@ struct PrankShareSheet: View {
     private func prankURL() -> URL {
         var comps = URLComponents()
         comps.scheme = "https"
-        comps.host = "prank.formevo.app"
-        comps.path = "/prank/"
+        comps.host = "share.formevo.app"
+        comps.path = "/gift/"
         comps.queryItems = [
             URLQueryItem(name: "pack", value: pack.id),
             URLQueryItem(name: "delay", value: "\(selectedDelay)"),
         ]
-        return comps.url ?? URL(string: "https://prank.formevo.app/prank/")!
+        return comps.url ?? URL(string: "https://share.formevo.app/gift/")!
     }
 }
